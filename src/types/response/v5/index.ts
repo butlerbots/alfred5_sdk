45867ba6917ef284f8e40e6e34a8f13efd606f94
FraglyG <hendrik@isdev.co>
2026-06-14 05:54:20 +0200

    fix: correct type exports to avoid collisions

--- src/types/response/v5/index.ts
+++ src/types/response/v5/index.ts
@@ -1,3 +1,20 @@
-export * from "./dialogue_response_v5";
-export * from "./ai_response_v5";
-export * from "./turn_registry_v5";
+export type {
+    RequestFailResponseV5,
+    RequestSuccessResponseV5,
+    RequestResponseV5,
+} from "./dialogue_response_v5";
+
+export type {
+    ConversationEvent,
+    ConversationEventCallback,
+    AIResponseV5,
+    EngineTurnFinishReason,
+    RoutingAttempt,
+    RoutingMetadata,
+} from "./ai_response_v5";
+
+export type {
+    TurnProgressEntryV5,
+} from "./turn_registry_v5";
+
+
